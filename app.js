5654ec9b7739a4fbd407edc512d4738895559e35
stefberto <stefunzky@gmail.com>
2020-06-28 20:07:41 -0400

    mongoose installed

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get("/", function(req, res){
   res.render("home");
